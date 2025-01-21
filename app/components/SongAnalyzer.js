920eacce7d7c4d36d9237f531f229565e963e874
Dennis Yang <sinned@gmail.com>
2025-01-21 10:13:03 -0800

    removing file

--- app/components/SongAnalyzer.js
+++
@@ -1,4 +0,0 @@
-let endpoint = '/api/analyze';
-if (mode === 'advanced') endpoint += '/advanced';
-else if (mode === 'freeplay') endpoint += '/freeplay';
-else if (mode === 'basic_freeplay') endpoint += '/basic-freeplay'; 
